2d8cb764cfb12715df806bd10d610859aaec3b3b
chendeping22 <954739202@qq.com>
2018-06-27 17:27:41 +0800

    test2

--- pages/expertList/expert.js
+++ pages/expertList/expert.js
@@ -38,30 +38,30 @@ Page({
     }
   },
   getHospital:function(){
-    let areaName = '';
-    console.log(this.data.region[2]);
-    if (this.data.region[2] == '全部') {
-      if (this.data.region[1] == '全部') {
-        areaName = this.data.region[0];
-      } else {
-        areaName = this.data.region[1];
-      }
-    } else {
-      areaName = this.data.region[2];
-    }
-    var apiUrl = `http://192.168.131.227:8080/dict/api/v1/getHospital?areaName=${areaName}`;
-    wx.request({
-      url: apiUrl, //仅为示例，并非真实的接口地址
-      header: {
-        'content-type': 'application/json' // 默认值
-      },
-      success: function (res) {
-        console.log(res.data)
-        that.setData({
-          hospital: res.data.data
-        })
-      }
-    })
+    // let areaName = '';
+    // console.log(this.data.region[2]);
+    // if (this.data.region[2] == '全部') {
+    //   if (this.data.region[1] == '全部') {
+    //     areaName = this.data.region[0];
+    //   } else {
+    //     areaName = this.data.region[1];
+    //   }
+    // } else {
+    //   areaName = this.data.region[2];
+    // }
+    // var apiUrl = `http://192.168.131.227:8080/dict/api/v1/getHospital?areaName=${areaName}`;
+    // wx.request({
+    //   url: apiUrl, //仅为示例，并非真实的接口地址
+    //   header: {
+    //     'content-type': 'application/json' // 默认值
+    //   },
+    //   success: function (res) {
+    //     console.log(res.data)
+    //     that.setData({
+    //       hospital: res.data.data
+    //     })
+    //   }
+    // })
   },
   /**
    * 生命周期函数--监听页面加载
