c5848bc0d3e9c36c3c56e8f1abbba24cd80c87b1
chendeping22 <954739202@qq.com>
2018-06-28 15:05:06 +0800

    update

--- pages/expertList/expert.js
+++ pages/expertList/expert.js
@@ -10,6 +10,7 @@ Page({
   data: { 
     customItem: '全部',
     hospital: [], 
+    department:[],
     doctorList:[
       { "doctorImg": "/image/icon-doctor.png", "doctorName": "兰建清", "department": "妇科", "position": "主任医师", "belong": "深圳第一医院", "level": "三甲医院", "skill": "妇科疾病诊断治疗", "price": "8.88" },
       { "doctorImg": "/image/icon-doctor.png", "doctorName": "罗腾可", "department": "放射科", "position": "主任医师", "belong": "北京大学深圳医院", "level": "三甲医院", "skill": "超声诊断", "price": "6.88" },
@@ -17,6 +18,7 @@ Page({
       ]
   
   },
+  //选择区域
   bindRegionChange: function (e) { 
     let that=this;
     this.setData({
@@ -37,31 +39,61 @@ Page({
      
     }
   },
-  getHospital:function(){
-    // let areaName = '';
-    // console.log(this.data.region[2]);
-    // if (this.data.region[2] == '全部') {
-    //   if (this.data.region[1] == '全部') {
-    //     areaName = this.data.region[0];
-    //   } else {
-    //     areaName = this.data.region[1];
-    //   }
-    // } else {
-    //   areaName = this.data.region[2];
-    // }
-    // var apiUrl = `http://192.168.131.227:8080/dict/api/v1/getHospital?areaName=${areaName}`;
-    // wx.request({
-    //   url: apiUrl, //仅为示例，并非真实的接口地址
-    //   header: {
-    //     'content-type': 'application/json' // 默认值
-    //   },
-    //   success: function (res) {
-    //     console.log(res.data)
-    //     that.setData({
-    //       hospital: res.data.data
-    //     })
-    //   }
-    // })
+  //选择医院
+  bindHospitalChange:function(){
+    this.getDepartment();
+    console.log("jin");
+  }, 
+  //选择科室 
+  bindDepartmentChange:function(){
+
+  }, 
+  //获取医院
+  getHospital:function(){ 
+    let that=this;
+    console.log(this.data.region[2]); 
+    let provice, city, area;  
+      provice = this.data.region[0]; 
+    if (this.data.region[1] == '全部') {
+      city = ''
+    } else {
+      city = this.data.region[1];
+    }
+    if (this.data.region[2]=='全部'){
+      area=''
+    }else{
+      area = this.data.region[2]; 
+    }
+    let apiUrl = `http://192.168.131.227:8080/dict/api/v1/getHospital?provice=${provice}&city=${city}&area=${area}&token=''`;
+    wx.request({
+      url: apiUrl, //仅为示例，并非真实的接口地址
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log(res.data)
+        that.setData({
+          hospital: res.data.data
+        })
+      }
+    })
+  },
+  //获取科室
+  getDepartment:function(){
+    let that=this;
+    let apiUrl = `http://192.168.131.227:8080/dict/api/v1/getDepartment?hospotalId=${that.data.hospital.id}&token=''`;
+    wx.request({
+      url: apiUrl, //仅为示例，并非真实的接口地址
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log(res.data) 
+        that.setData({
+          department:res.data.data
+        })
+      }
+    })
   },
   /**
    * 生命周期函数--监听页面加载
@@ -98,7 +130,7 @@ Page({
         });
       }
     })
-   
+    this.getDepartment();
   },
 
   /**
