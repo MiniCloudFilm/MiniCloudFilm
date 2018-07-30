5b505814500569c2d354793bff8f853451fdc473
chendeping22 <954739202@qq.com>
2018-07-30 16:11:28 +0800

    上传视频上拉加载

--- pages/film/film.js
+++ pages/film/film.js
@@ -1,4 +1,3 @@
-var sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
 var app = getApp();
 Page({
   data: {
@@ -7,26 +6,32 @@ Page({
     activeIndex: 0,
     check: 0,
     isFree: true,
-    isPay: true,
-    user: wx.getStorageSync('userList'),
+    isPay: true, 
     isBuy: false,
     hidden:false,
     url: app.globalData.api.url,
     free: app.globalData.api.film.getFreeVideo,
-    charge: app.globalData.api.film.getChargeVideo,
-    
+    charge: app.globalData.api.film.getChargeVideo, 
   },
   tabClick: function (e) {
+    wx.showLoading({
+      title: '加载中..',
+    })
     this.setData({
       sliderOffset: e.currentTarget.offsetLeft,
       activeIndex: e.currentTarget.id,
-      check: e.currentTarget.dataset.index
+      check: e.currentTarget.dataset.index,
+      videoList:[],
+      page: 1,
+      isHideLoadMore: true,
+      isLoad: true,
+      isEnd: true
     });
     // console.log(this.data.check);
     if (e.currentTarget.dataset.index == '0') {
-      this.getFreeVideo();
+      this.getVideo(this.data.free);
     } else if (e.currentTarget.dataset.index == '1') {
-      this.getChargeVideo();
+      this.getVideo(this.data.charge);
     } else if (e.currentTarget.dataset.index == '2') {
       this.getQueryVideoLog();
     }
@@ -40,12 +45,83 @@ Page({
     }
   },
   //获取视频
-  getVideo:function(api){
-    this.data.videoList = [];
+  getVideo:function(api){ 
     wx.request({
       // url: app.globalData.api.film.getChargeVideo,
       url: api,
       data: {
+        "userId":this.data.user.userId,
+        "page": this.data.page,
+        "token": this.data.token
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: res => {
+        console.log(res.data)
+        if (res.data.code == "200") {
+          let tmpArr;
+          if (this.data.page > 1) {
+            tmpArr = this.data.videoList;
+          } else {
+            tmpArr = [];
+            this.setData({
+              isEnd: true
+            })
+          }
+          // 这一步实现了上拉加载更多
+          tmpArr.push.apply(tmpArr, res.data.data.datas);
+          this.setData({
+            videoList: tmpArr
+          })
+          if (res.data.data.datas.length == 0) {
+            this.setData({
+              isLoad: false,
+            })
+            if (this.data.page > 1) {
+              this.setData({
+                isEnd: true
+              })
+            }
+          } else {
+            if (res.data.data.datas.length < 15) {
+              this.setData({
+                isLoad: false,
+                isHideLoadMore: true
+              })
+              if (this.data.page > 1) {
+                this.setData({
+                  isEnd: false
+                })
+              }
+            } else {
+              this.setData({
+                isLoad: true,
+                isHideLoadMore: true,
+                page: ++this.data.page
+              })
+            }
+          } 
+        }
+        console.log(this.data.videoList);
+        wx.hideLoading()
+      },
+      fail:res=>{
+        wx.hideLoading()
+        wx.showToast({
+          title: '服务器异常，请稍后再试！',
+          icon: 'none',
+          duration: 2000
+        })
+      }
+    })
+  }, 
+  //获取观看记录
+  getQueryVideoLog: function() { 
+    wx.request({ 
+      url: app.globalData.api.film.getQueryVideoLog, 
+      data: {
+        "userId": this.data.user.userId,
         "page": this.data.page,
         "token": this.data.token
       },
@@ -97,128 +173,17 @@ Page({
               })
             }
           }
-          this.setData({
-            videoList: res.data.data.datas
-          })
         }
+        console.log(this.data.videoList);
         wx.hideLoading()
       },
-      fail:res=>{
+      fail: res => {
         wx.hideLoading()
         wx.showToast({
           title: '服务器异常，请稍后再试！',
           icon: 'none',
           duration: 2000
         })
-      }
-    })
-  },
-  //查看收费视频
-  getChargeVideo: function() {
-    this.data.videoList = [];
-    wx.request({ 
-      url: app.globalData.api.film.getChargeVideo, 
-      data: {
-        "page": 1,
-        "token": this.data.token
-      },
-      header: {
-        'content-type': 'application/json' // 默认值
-      },
-      success: res => {
-        console.log(res.data)
-        if (res.data.code == "200") {
-          let tmpArr;
-          if (page > 1) {
-            tmpArr = this.data.videoList;
-          } else {
-            tmpArr = [];
-            this.setData({
-              isEnd: true
-            })
-          }
-          // 这一步实现了上拉加载更多
-          tmpArr.push.apply(tmpArr, res.data.data.datas);
-          this.setData({
-            videoList: tmpArr
-          })
-          if (res.data.data.datas.length == 0) {
-            this.setData({
-              isLoad: false,
-            })
-            if (page > 1) {
-              this.setData({
-                isEnd: true
-              })
-            }
-          } else {
-            if (res.data.data.datas.length < 15) {
-              this.setData({
-                isLoad: false,
-                isHideLoadMore: true
-              })
-              if (page > 1) {
-                this.setData({
-                  isEnd: false
-                })
-              }
-            } else {
-              this.setData({
-                isLoad: true,
-                isHideLoadMore: true,
-                page: ++page
-              })
-            }
-          }
-          this.setData({
-            videoList: res.data.data.datas
-          })
-        }
-      }
-    })
-  },
-  //查看免费视频
-  getFreeVideo: function() {
-    this.data.videoList = [];
-    wx.request({ 
-      url: app.globalData.api.film.getFreeVideo, 
-      data: {
-        "page": 1,
-        "token": this.data.token
-      },
-      header: {
-        'content-type': 'application/json' // 默认值
-      },
-      success: res => {
-        console.log(res.data)
-        if (res.data.code == "200") {
-          this.setData({
-            videoList: res.data.data.datas
-          }) 
-        }
-      }
-    })
-  },
-  //获取观看记录
-  getQueryVideoLog: function() {
-    this.data.videoList = [];
-    wx.request({ 
-      url: app.globalData.api.film.getQueryVideoLog, 
-      data: {
-        "userId": this.data.user.userId,
-        "page": 1,
-        "token": this.data.token
-      },
-      header: {
-        'content-type': 'application/json' // 默认值
-      },
-      success: res => {
-        console.log(res.data)
-        if (res.data.code == "200") {
-          this.setData({
-            videoList: res.data.data.datas
-          })
-        }
       }
     })
   },
@@ -277,9 +242,10 @@ Page({
           if (!this.data.isBuy) { 
             wx.showModal({
               title: '提示',
-              content: `确定购买${data.title}，该视频！`,
-              success: function (res) {
-                if (res.confirm) {
+              content: `是否购买《${data.title}》?`,
+              success: res=> {
+                if (res.confirm) { 
+                  this.getSaveVideoLog(data);
                   wx.navigateTo({
                     url: `../confirmPay/confirmPay?videoId=${data.videoId}&charge=${data.charge}&title=${data.title}&upId=${data.userId}&type=2`
                   })
@@ -295,25 +261,25 @@ Page({
         }
       }
     })
-  },
+  }, 
   onLoad: function() {
     wx.showLoading({
       title: '加载中..',
     })
-    app.checkLoginInfo(app.getCurrentUrl()); 
     this.setData({
       token: app.globalData.token,
       user: app.globalData.userList,
       isHideLoadMore: true,
-      page: 1,
       isLoad: true,
       isEnd: true,
+      page: 1,
     })
+    app.checkLoginInfo(app.getCurrentUrl());  
     this.getVideo(this.data.free);  
   },
    //监听页面滚动
   onPageScroll: function (e) { 
-    if (e.scrollTop > 0) {
+    if (e.scrollTop > 10) {
       this.setData({
         fixed: true
       })
@@ -328,6 +294,11 @@ Page({
    */
   onPullDownRefresh: function () {
     wx.showNavigationBarLoading() //在标题栏中显示加载  
+    this.setData({
+      page: 1,
+      isEnd: true,
+      isLoad: true
+    })
     setTimeout(() => {
       if (this.data.check == '0') {
         this.getVideo(this.data.free)
@@ -336,11 +307,6 @@ Page({
       } else if (this.data.check == '2') {
         this.getQueryVideoLog();
       } 
-      this.setData({
-        page: 1,
-        isEnd: true,
-        isLoad: true
-      })
       wx.hideNavigationBarLoading() //完成停止加载
       wx.stopPullDownRefresh() //停止下拉刷新 
     }, 1500);
